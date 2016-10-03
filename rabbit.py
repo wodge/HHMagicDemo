1eeaf902ca9e9c00ef672422ca8f544086cdf9dd
Paul <paul@noworriesit.co.uk>
2016-10-03 19:54:49 +0100

    simple rabbit trick

---
+++ rabbit.py
@@ -0,0 +1,24 @@
+def pullrabbit():
+ print(" /\ ")
+ print(" //\\ /\ ")
+ print(" /' \\ //\\ ")
+ print(" \\ // `\ ")
+ print(" \\ // ")
+ print(" .-'^'-. ")
+ print(" .' a___a `. ")
+ print(" == (___) == ")
+ print(" '. ._I_. .' ")
+ print(" ____/.`-----'.\____ ")
+ print(" [###(__)#####(__)###]")
+ print(" ~~|#############|~~ ")
+ print(" |#############| ")
+ print(" |#############| ")
+ print(" |#############| ")
+ print(" |#############| ")
+ print(" ~~~~~~~~~~~~~~~ ")
+magicword = input("Enter the magic word: ")
+if magicword == "abracadabra":
+ pullrabbit()
+else:
+ print ("That's not a magic word")
+
